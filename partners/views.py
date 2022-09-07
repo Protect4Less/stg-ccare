b60dcf5643d9f11f8ca75999bedd4ca7353e7093
anandp4l <shanmuganand.nadar@protect4less.com>
2022-09-07 15:33:41 +0530

    Partner 1062 added

--- partners/views.py
+++ partners/views.py
@@ -47,6 +47,7 @@ def upload_create_policy(request):
         {"id": 1057, "name": '1057 - Asia Palace Mobile Phones LLC'},
         {"id": 1060, "name": '1060 - MASTERPIECE LLC'},
         {"id": 1061, "name": '1061 - Desert Beat Trading LLC'},
+        {"id": 1062, "name": '1062 - GEANT HYPERMARKET'},
         ]
 
     if excel_file is not None and partner_code is not None:
@@ -262,7 +263,7 @@ def upload_create_policy(request):
                 xcqc_id_cell = "{}{}".format(xcqc_id_col, row_number )
                 xcqc_id_value =  str(worksheet[xcqc_id_cell].value)
 
-            if row_number != 1 and partner_code in ['1026','1030','1031', '1025', '1014', '1038','1033','1041','1036','1044','1035','1026','1046', '1051','1049','1052','1040','1053','1064','1054', '1055', '1056', '1057', '1060', '1061'] and email_value != 'None':
+            if row_number != 1 and partner_code in ['1026','1030','1031', '1025', '1014', '1038','1033','1041','1036','1044','1035','1026','1046', '1051','1049','1052','1040','1053','1064','1054', '1055', '1056', '1057', '1060', '1061','1062'] and email_value != 'None':
 
                 sku_value = sku_value if sku_value is not None and sku_value != "None" else ""
                 popd_data = PartnersDAO.insert_partners_offline_policy_data({
