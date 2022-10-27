0327ddc1cfa929184223f08a2c10f93f34a90812
Protect4Less <support@protect4less.com>
2022-10-27 09:45:15 -0400

    new html for renewal policy.

--- partners/urls.py
+++ partners/urls.py
@@ -6,4 +6,5 @@ app_name = 'partners'
 
 urlpatterns = [
     path('upload-create-policy/', views.upload_create_policy, name='upload-create-policy'),
+    path('upload_renewal_policy/', views.upload_renewal_policy, name='upload_renewal_policy')
 ]
